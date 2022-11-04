6e663f0d5a04019642f0456c228da6ad06021c07
zedd11 <dangocphuong11t3@gmail.com>
2022-11-04 17:10:34 +0700

    Update code

---
+++ app/src/main/java/example/android/exam/exam7compose/app/ExamApp.kt
@@ -0,0 +1,12 @@
+package example.android.exam.exam7compose.app
+
+import android.app.Application
+
+class ExamApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setupInitApp()
+    }
+
+    private fun setupInitApp() {}
+}
